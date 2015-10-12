d0e3de5840f7c5b91f782b0daccbea20d40cef9b
mona17493 <mona17493@gmail.com>
2015-10-12 14:53:54 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-get "/contacts" do
-  @contacts = Contact.all
+get "/accounts" do
+  @accounts = Account.all
   erb :index
 end
 
